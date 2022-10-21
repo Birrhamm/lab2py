9ba3b116b577ecd2c32563eed597e868c551bd68
Birrham <birrhamefendi180717@gmail.com>
2022-10-21 10:19:32 +0700

    latihan python

---
+++ latihan3.py
@@ -0,0 +1,16 @@
+#input nilai variabel
+a=input ("masukan nilai a:")
+b=input ("masukan nilai b:")
+
+# cetak niai variabel
+print("variabel a=",a)
+print("variabel b=",b)
+
+# vetak hasil operasi kedua variabel dengan string Format
+print("Hasil penggabungan (1)&(0)=%s".format(a+b) %(a=b))
+
+#konversi nilai variabel
+a=int(a)
+b=int(b)
+print("Hasil penjumlahan (1)+(0)=%d".format(a,b) %(a+b))
+print("Hasil pembagian (1)/(0)=%d".format (a,b) %(a/b))
